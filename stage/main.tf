eca2dd9a469a5a831a1417322724e6ffdb40c9d9
Brendan Keane <btkeane@gmail.com>
2025-05-25 10:32:19 -0700

    fix the error

--- stage/main.tf
+++ stage/main.tf
@@ -2,7 +2,6 @@ locals {
     account_id = data.aws_caller_identity.current.account_id
     account_name = var.topology.account_lookup[local.account_id]
     account_region = data.aws_region.current.name
-
     resources = var.topology.resources[local.account_name][var.stage]
 }
 
